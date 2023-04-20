5837a8b798c2bae969b98620f8b1eabaad7d94f4
0xlkda <lekhacduyanh@gmail.com>
2023-04-20 16:24:24 +0700

    create hover visual for text

--- config/text.js
+++ config/text.js
@@ -1,9 +1,42 @@
-import { decorate, lockMovement } from '@/libs/fabric'
+import COLORS from '@/assets/colors'
+import { render, decorate, lockMovement } from '@/libs/fabric'
 
 const handPointer = (i) => i.set({ hoverCursor: 'pointer' })
+const normalize = (i) => i.set({ stroke: 'none', strokeWidth: 0 })
+const highlight = (i) => i.set({ stroke: COLORS.primary, strokeWidth: 2 })
+
+const handleMouseOut = (e) => render(normalize(e.target))
+const handleMouseOver = (e) => render(highlight(e.target))
+
+function enableMouseOver(i) {
+  i.on('mouseover', handleMouseOver)
+  i.on('mouseout', handleMouseOut)
+  return i
+}
+
+function disableMouseOver(i) {
+  i.off('mouseover')
+  i.off('mouseout')
+  return i
+}
 
 export function applyTextConfig(obj) {
   const item = decorate(obj, [handPointer, lockMovement])
-  item.on('selected', () => item.notify())
+
+  // events setup 
+  enableMouseOver(item)
+
+  item.on({
+    'selected': () => {
+      item.notify()
+      disableMouseOver(item)
+      render(normalize(item))
+    },
+    'deselected': () => {
+      enableMouseOver(item)
+      render(normalize(item))
+    }
+  })
+
   return item
 }
